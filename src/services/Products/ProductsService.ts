97becf6b623b24c18e6c4101879b8b9502bc35b5
david <ruscuta.daavid@gmail.com>
2022-10-01 15:56:35 +0300

    MAJ

--- src/services/Products/ProductsService.ts
+++
@@ -1,7 +0,0 @@
-import { api } from "../ServiceHelper";
-
-export const fetchProductData = (id) => {
-  await api.get("/products/" + id).then((response) => {
-    console.log(response.data);
-  });
-};
